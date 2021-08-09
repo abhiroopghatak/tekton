52ff19fbad8a8e6e4175023ca50689a6fc03eb72
abhiroopghatak <ghatak.20@gmail.com>
2021-08-09 09:29:37 +0000

    filter ,search , pagination applied in resource page

--- react-frontend/src/components/routes/namespaces/Namespaces.js
+++ react-frontend/src/components/routes/namespaces/Namespaces.js
@@ -6,35 +6,23 @@ import DataService from '../../../restapi/data-service/DataService.js';
 import '../../../styles/componentstyles/table.css';
 
 
-import { useTable } from "react-table";
-
-const tableHead = {
-	name: "Campaign Name",
-	parentId: "Campaign Id",
-	campaignType: "Type",
-	status: "Status",
-	channel: "Channel"
-};
+import { useTable, useSortBy, useFilters, useGlobalFilter, useAsyncDebounce, usePagination } from "react-table";
 
 
 
 const Namespaces = (props) => {
 
 	const [resources, setResources] = useState([]);
-	const [searchTitle, setSearchTitle] = useState("");
-	const resourcesRef = useRef();
-	resourcesRef.current = resources;
 
-	useEffect( async () => {		retrieveResources();	}, []);
+	useEffect(async () => { retrieveResources(); }, []);
 
-	const onChangeSearchTitle = (e) => {
-		const searchTitle = e.target.value;
-		setSearchTitle(searchTitle);
-	};
+
 	const retrieveResources = () => {
-		
-		DataService.getResourcePerNamespaces({clusterId: "1111",
-			userId: "1111"})
+
+		DataService.getResourcePerNamespaces({
+			clusterId: "1111",
+			userId: "1111"
+		})
 			.then((response) => {
 				setResources(response.data);
 			})
@@ -43,29 +31,63 @@ const Namespaces = (props) => {
 			});
 	};
 
-	const refreshList = () => {
-		retrieveResources();
+
+	// Define a default UI for filtering
+	function GlobalFilter({
+		preGlobalFilteredRows,
+		globalFilter,
+		setGlobalFilter,
+	}) {
+		const count = preGlobalFilteredRows.length
+		const [value, setValue] = React.useState(globalFilter)
+		const onChange = useAsyncDebounce(value => {
+			setGlobalFilter(value || undefined)
+		}, 200)
+
+		return (
+			<span>
+				Search:{' '}
+				<input
+					value={value || ""}
+					onChange={e => {
+						setValue(e.target.value);
+						onChange(e.target.value);
+					}}
+					placeholder={`${count} records...`}
+					style={{
+						fontSize: '1.1rem',
+						border: '0',
+					}}
+				/>
+			</span>
+		)
+	}
+
+
+
+
+	const defaultColumn = React.useMemo(
+		() => ({
+			Filter: DefaultColumnFilter
+		}),
+		[]
+	);
+
+	function DefaultColumnFilter({
+		column: { filterValue, preFilteredRows, setFilter },
+	}) {
+		const count = preFilteredRows.length;
+
+		return (
+			<input
+				value={filterValue || ""}
+				onChange={(e) => {
+					setFilter(e.target.value || undefined); // Set undefined to remove the filter entirely
+				}}
+				placeholder={`Search ${count} records...`}
+			/>
+		);
 	};
-
-
-
-	const findByTitle = () => {
-		DataService.getCostDataPerCluster(1111)
-			.then((response) => {
-				setResources(response.data);
-			})
-			.catch((e) => {
-				console.log(e);
-			});
-	};
-
-	const openTutorial = (rowIndex) => {
-		const id = resourcesRef.current[rowIndex].id;
-
-		props.history.push("/Resources/" + id);
-	};
-
-
 
 	const columns = useMemo(
 		() => [
@@ -80,116 +102,145 @@ const Namespaces = (props) => {
 			{
 				Header: "Storage",
 				accessor: "storageVolume",
-				
+				disableFilters: true,
+
 			},
 			{
 				Header: "cpu-quota",
 				accessor: "requestCpu",
+				disableFilters: true,
 			},
 			{
 				Header: "cpu-used",
 				accessor: "usedCpu",
+				disableFilters: true,
 			},
 			{
 				Header: "memory-quota",
 				accessor: "requestMemory",
+				disableFilters: true,
 			},
 			{
 				Header: "memory-used",
 				accessor: "usedMemory",
+				disableFilters: true,
 			},
-			{
-				Header: "Actions",
-				accessor: "actions",
-				Cell: (props) => {
-					const rowIdx = props.row.id;
-					return (
-						<div>
-							<span onClick={() => openTutorial(rowIdx)}>
-								<i className="far fa-edit action mr-2"></i>
-							</span>
 
-							<span >
-								<i className="fas fa-trash action"></i>
-							</span>
-						</div>
-					);
-				},
-			},
 		],
+		[]
+	);
+	const filterTypes = React.useMemo(
+		() => ({
+			text: (rows, id, filterValue) => {
+				return rows.filter((row) => {
+					const rowValue = row.values[id];
+					return rowValue !== undefined
+						? String(rowValue)
+							.toLowerCase()
+							.startsWith(String(filterValue).toLowerCase())
+						: true;
+				});
+			}
+		}),
 		[]
 	);
 	const {
 		getTableProps,
 		getTableBodyProps,
 		headerGroups,
-		rows,
-		prepareRow,
+		prepareRow, state, visibleColumns,
+		preGlobalFilteredRows,
+		setGlobalFilter, page,
+
+		canPreviousPage,
+		canNextPage,
+		pageOptions,
+		pageCount,
+		gotoPage,
+		nextPage,
+		previousPage,
+		setPageSize,
+		state: { pageIndex, pageSize },
 	} = useTable({
 		columns,
-		data: resources,
-	});
+		data: resources,initialState: { pageSize:8 , pageIndex: 0 }, defaultColumn, filterTypes
+	}, useFilters, useGlobalFilter,useSortBy,usePagination);
 
 	return (
 		<div className="list row">
-			<div className="col-md-8">
-				<div className="input-group mb-3">
-					<input
-						type="text"
-						className="form-control"
-						placeholder="Search by title"
-						value={searchTitle}
-						onChange={onChangeSearchTitle}
-					/>
-					<div className="input-group-append">
-						<button
-							className="btn btn-outline-secondary"
-							type="button"
-							onClick={findByTitle}
+			
+			<div className="col-md-12 list">
+				<h3> Accessbile Namespaces and resources in use</h3>
+				<table className="table  table-striped table-bordered" {...getTableProps()}>
+					<thead>
+						<th
+							colSpan={visibleColumns.length}
+							style={{
+								textAlign: 'left',
+							}}
 						>
-							Search
-            </button>
-					</div>
+							<GlobalFilter
+								preGlobalFilteredRows={preGlobalFilteredRows}
+								globalFilter={state.globalFilter}
+								setGlobalFilter={setGlobalFilter}
+							/>
+						</th>
+						{headerGroups.map((headerGroup) => (
+							<tr {...headerGroup.getHeaderGroupProps()}> {headerGroup.headers.map((column) => (
+								<th {...column.getHeaderProps(column.getSortByToggleProps())}> {column.render("Header")} <span>{
+									column.isSorted
+										? column.isSortedDesc
+											? ' 🔽'
+											: ' 🔼'
+										: ''
+								}</span>
+								</th>))} </tr>))}
+
+					</thead>
+					<tbody {...getTableBodyProps()}> {page.map((row, i) => {
+						prepareRow(row); return (
+							<tr {...row.getRowProps()}> {row.cells.map((cell) => {
+								return (
+									<td {...cell.getCellProps()}>{cell.render("Cell")}</td>);
+							})} </tr>);
+					})} </tbody>
+				</table>
+				
+				<div className="pagination">
+					<button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+						{'<<'}
+					</button>{' '}
+					<button onClick={() => previousPage()} disabled={!canPreviousPage}>
+						{'<'}
+					</button>{' '}
+					<button onClick={() => nextPage()} disabled={!canNextPage}>
+						{'>'}
+					</button>{' '}
+					<button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+						{'>>'}
+					</button>{' '}
+					<span>
+						Page{' '}
+						<strong>
+							{pageIndex + 1} of {pageOptions.length}
+						</strong>{' '}
+					</span>
+					{' '}
+					<select
+						value={pageSize}
+						onChange={e => {
+							setPageSize(Number(e.target.value))
+						}}
+					>
+						{[8,12,16,20].map(pageSize => (
+							<option key={pageSize} value={pageSize}>
+								Show {pageSize}
+							</option>
+						))}
+					</select>
 				</div>
 			</div>
-			<div className="col-md-12 list">
-				<table
-					className="table table-striped table-bordered"
-					{...getTableProps()}
-				>
-					<thead>
-						{headerGroups.map((headerGroup) => (
-							<tr {...headerGroup.getHeaderGroupProps()}>
-								{headerGroup.headers.map((column) => (
-									<th {...column.getHeaderProps()}>
-										{column.render("Header")}
-									</th>
-								))}
-							</tr>
-						))}
-					</thead>
-					<tbody {...getTableBodyProps()}>
-						{rows.map((row, i) => {
-							prepareRow(row);
-							return (
-								<tr {...row.getRowProps()}>
-									{row.cells.map((cell) => {
-										return (
-											<td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-										);
-									})}
-								</tr>
-							);
-						})}
-					</tbody>
-				</table>
-			</div>
 
-			<div className="col-md-8">
-				<button className="btn btn-sm btn-danger" >
-					Remove All-todelete
-        </button>
-			</div>
 		</div>
 	);
 };
